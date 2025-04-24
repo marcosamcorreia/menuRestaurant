6404b33f87a56f6d6be7e82049f8a51ecc5d9bb5
Marcos <marcosamcorreia2006@gmail.com>
2025-04-24 18:29:59 -0300

    Rename test.py to menu.py

---
+++ menu.py
@@ -0,0 +1,18 @@
+a=0
+
+
+
+test={111: "Pizza",
+      222:"Hamburguer",
+      333: "Refri"}
+vlr={111:2.5,
+     222:3.8,
+     333:12.2}
+
+#a=int(input("Escreva")) 
+while a!=2:
+    print("oi")
+    a=int(input("Escreva"))
+    print(a)
+    print(test[a])
+    print(vlr[a])
