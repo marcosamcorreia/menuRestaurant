9c59f12e44d2a0d28ccd2ebc56acdd1ac4650d18
Marcos <marcosamcorreia2006@gmail.com>
2025-04-25 16:34:32 -0300

    Create the items list

--- menu.py
+++ menu.py
@@ -1,18 +1,27 @@
 a=0
+i=0
 
 
+items=[
+    [101,"The OG Cheeseburger",7.99],
+    [102,"The Double Trouble",10.99],
+    [103,"The Backyard BBQ",11.49],
+    [201,"The Volcano Burger",9.99],
+    [202,"The Truffle Deluxe",12.99],
+    [203,"The Hawaiian Wave",10.49],
+    [301,"Crispy Chicken Crunch",8.99],
+    [302,"Grilled Perfection",9.49],
+    [303,"The Ultimate Veggie",9.99],
+    [401,"Classic Fries",3.99],
+    [402,"Loaded Cheese Fries",5.99],
+    [403,"Onion Rings",4.99],
+    [501,"Hand-Spun Milkshakes",4.99],
+    [502,"Craft Sodas",2.49],
+    [503,"Churro Bites",5.49]    
+]
+print(len(items)) #quantity items 
 
-test={111: "Pizza",
-      222:"Hamburguer",
-      333: "Refri"}
-vlr={111:2.5,
-     222:3.8,
-     333:12.2}
+while a<len(items):
+    print(items[a])
+    a=a+1
 
-#a=int(input("Escreva")) 
-while a!=2:
-    print("oi")
-    a=int(input("Escreva"))
-    print(a)
-    print(test[a])
-    print(vlr[a])
